edb98e60964bf7b719e3e7c9a699e660632f9a52
Servando Reyes <aprende.conociendo@gmail.com>
2018-02-21 00:07:06 -0300

    Changes on example paths

--- src/main/java/com/prueba/enums/ResourcesPath.java
+++ src/main/java/com/prueba/enums/ResourcesPath.java
@@ -2,9 +2,9 @@ package com.prueba.enums;
 
 public enum ResourcesPath {
 
-    MOVIMIENTOS("/Users/andres/IdeaProjects/CuentasBancarias/src/main/resources/movimientos/"),
-    TITULARES("/Users/andres/IdeaProjects/CuentasBancarias/src/main/resources/titulares/"),
-    CUENTAS("/Users/andres/IdeaProjects/CuentasBancarias/src/main/resources/cuentas/");
+    MOVIMIENTOS("/Users/github/IdeaProjects/CuentasBancarias/src/main/resources/movimientos/"),
+    TITULARES("/Users/github/IdeaProjects/CuentasBancarias/src/main/resources/titulares/"),
+    CUENTAS("/Users/github/IdeaProjects/CuentasBancarias/src/main/resources/cuentas/");
 
     public String path;
 
